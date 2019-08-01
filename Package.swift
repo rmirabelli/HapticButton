2a773316a7c68924c50356a75f6311d5011ad0da
russellm <russell@mirabellifamily.org>
2019-08-01 15:04:17 -0500

    working towards a build

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "HapticButton",
-            targets: ["HapticButton", "HapticEvent"]),
+            targets: ["HapticButton"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
